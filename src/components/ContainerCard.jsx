d4ae05db66eedd72f9161647b837bd7078596a68
Burak <burakturk@gmail.com>
2023-09-21 11:36:15 +0300

    playerCard

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -4,31 +4,29 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useState } from "react";
+import { PlayerCard } from "react";
 import { data } from "../helpers/data";
 
 const ContainerCard = () => {
   const [search, setSearch] = useState("");
+  console.log(data);
   const handleChange = (e) => {
     console.log(e.target);
     setSearch(e.target.value);
     console.log(search);
   };
-  // const handleChange = (e) => {
-  //   console.log(e.target.value);
-  //   setSearch(e.target.value);
-  // };
-  //   console.log(search);
 
-  // const filteredData = data.filter((item) =>
-  //   item.name.toLowerCase().includes(search.trim().toLowerCase())
-  // );
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
 
-  // console.log("filter", filteredData);
+  console.log("filter", filteredData);
 
   return (
     <>
       <Form.Control
         type="search"
+        value={search}
         placeholder="Search Player..."
         onChange={handleChange}
       />
@@ -36,10 +34,18 @@ const ContainerCard = () => {
         className="p-3 rounded-4 card-container my-3"
         // onChange={handleChange}
       />
-      <Row xs={2} md={4} lg={6} className="justify-content-center my-g-3">
+      <Row xs={2} sm={2} md={3} lg={4} className="justify-content-center g-3">
         <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-        <Col>3 of 3</Col>
+        {filteredData.map((player, i) => (
+          // <PlayerCard
+          //   key={i}
+          //   player={player}
+          //   img={player.img}
+          //   name={player.name}
+          //   statistics={player.statistics}
+          //   {...player}
+          <PlayerCard key={i} {...player} />
+        ))}
       </Row>
       <Container />
     </>
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
-const PlayerCard = () => {
+import Col from "react-bootstrap/Col";
+const PlayerCard = (name,image,statistics) => {
   return (
-    <div>PlayerCard</div>
+    <Col> <Card onClick={()=>setShowImage(!showImage)} className="player-card" role="button">
+    {showImage ? (
+      <Card.Img
+        variant="top"
+        src={img}
+        height="350px"
+        // onClick={() => setShowImage(false)}
+      />
+    ) : (
+      <ul 
+      className="m-auto"
+    //   onClick={() => setShowImage(true)}
+      >
+        {statistics.map((item, i) => (
+          <li className="list-unstyled h5 text-start" key={i}>
+            {" "}
+            🏀 {item}
+          </li>
+        ))}
+      </ul>
+    )}
+    <Card.Footer>
+      <Card.Title>{name}</Card.Title>
+    </Card.Footer>
+  </Card></Col>;
   )
 }
 
